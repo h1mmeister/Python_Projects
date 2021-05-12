53a8fda1dcb8c17efc892c7291242e0e1a7e4fbd
h1m <himanshu.yadav643@gmail.com>
2021-05-13 04:52:43 +0530

    added geo.py class with class and instance variables and methods

--- App_1/world.py
+++ App_1/world.py
@@ -1,9 +1,10 @@
 from folium import Map
+from geo import Geopoint
 
+# Get Latitude and Longitude values
 latitude = 40.09
 longitude = -3.47
 
-# dir(int) and dir(__builtins__)
 antipode_latitude = latitude * (-1)
 
 if longitude <= 0:
@@ -16,5 +17,10 @@ location = [antipode_latitude, antipode_longitude]
 world_map = Map(location)
 world_map.save("map.html")
 
+my_point = Geopoint(antipode_latitude, antipode_longitude)
+print(my_point.closest_parallel())
+
 print(antipode_latitude)
 print(antipode_longitude)
+
+# dir(int) to see all the methods and dir(__builtins__) to see all builtins functions
